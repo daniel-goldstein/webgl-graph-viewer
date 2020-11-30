6e4458432418f8fc52f920f07d6ed5e27d5059f8
Daniel Goldstein <danielgold95@gmail.com>
2020-11-30 01:15:23 -0500

    animate randomizing node positions

--- public/js/three-graph-viewer.js
+++ public/js/three-graph-viewer.js
@@ -1,17 +1,25 @@
 /* eslint no-use-before-define: 0 */
 
-import * as THREE from "./build/three.module.js";
-import { Interaction } from "./build/three-interaction.module.js";
-import { OrbitControls } from "./examples/jsm/controls/OrbitControls.js";
+import * as THREE from './build/three.module.js';
+import { Interaction } from './build/three-interaction.module.js';
+import { OrbitControls } from './examples/jsm/controls/OrbitControls.js';
+import sample from '../node_modules/underscore/modules/sample.js';
+import terrainData from '../terrain.js';
 
-const SPHERE_RADIUS = 5;
-const CYLINDER_RADIUS = 1;
+const SPHERE_RADIUS = 1;
+const CYLINDER_RADIUS = 0.2;
+
+const GRAPH_BOUNDARIES = {
+  x: 50,
+  y: 40,
+  z: 50,
+};
 
 const Y_AXIS = new THREE.Vector3(0, 1, 0);
 
-const BACKGROUND_FILEPATH = "space.jpeg";
-const STAINLESS_STEEL_FILEPATH = "stainless-steel.jpg";
-const LAVA_FILEPATH = "lava.jpg";
+const BACKGROUND_FILEPATH = 'space.jpeg';
+const STAINLESS_STEEL_FILEPATH = 'stainless-steel.jpg';
+const LAVA_FILEPATH = 'lava.jpg';
 
 const BLUE = 0x1987f8;
 const RED = 0xe9533b;
@@ -28,7 +36,7 @@ const NODE_TEXTURES = [
 
 const exampleGraph = {
   A: {
-    neighbors: ["B", "C"],
+    neighbors: ['B', 'C', 'D', 'E'],
     posn: {
       x: 10,
       y: 10,
@@ -37,7 +45,7 @@ const exampleGraph = {
     texture: NODE_TEXTURES[0],
   },
   B: {
-    neighbors: ["C"],
+    neighbors: ['C', 'E'],
     posn: {
       x: -10,
       y: 10,
@@ -54,13 +62,104 @@ const exampleGraph = {
     },
     texture: NODE_TEXTURES[0],
   },
+  D: {
+    neighbors: [],
+    posn: {
+      x: 10,
+      y: 0,
+      z: -5,
+    },
+    texture: NODE_TEXTURES[0],
+  },
+  E: {
+    neighbors: [],
+    posn: {
+      x: -15,
+      y: 20,
+      z: -10,
+    },
+    texture: NODE_TEXTURES[0],
+  },
 };
 
+function generateRandomGraph(numNodes) {
+  const nodeIDs = new Array(numNodes);
+  for (let i = 0; i < numNodes; i++) {
+    nodeIDs[i] = i;
+  }
+
+  return nodeIDs.reduce((graph, nodeID) => {
+    graph[nodeID] = {
+      posn: randomPosition(),
+      neighbors: randomNeighbors(nodeIDs),
+      texture: NODE_TEXTURES[0],
+    };
+    return graph;
+  }, {});
+}
+
+function randomPosition() {
+  return {
+    x: Math.random() * GRAPH_BOUNDARIES.x - GRAPH_BOUNDARIES.x / 2,
+    y: Math.random() * GRAPH_BOUNDARIES.y,
+    z: Math.random() * GRAPH_BOUNDARIES.z - GRAPH_BOUNDARIES.z / 2,
+  };
+}
+
+function randomNeighbors(nodeIDs) {
+  const numNeighbors = Math.floor(Math.random() * nodeIDs.length);
+  if (numNeighbors === 1) {
+    return [sample(nodeIDs)];
+  }
+  return sample(nodeIDs, numNeighbors);
+}
+
+function getNodePositions(graph) {
+  let positions = {};
+  Object.entries(graph).forEach(([nodeID, node]) => {
+    positions[nodeID] = node.posn;
+  });
+  return positions;
+}
+
+function randomNodePositions(graph) {
+  let positions = {};
+  Object.entries(graph).forEach(([nodeID, _]) => {
+    positions[nodeID] = randomPosition();
+  });
+  return positions;
+}
+
+function* randomizeGraphAnimation(graph) {
+  const nodeStartingPositions = getNodePositions(graph);
+  const nodeDestinations = randomNodePositions(graph);
+  const numFrames = 20;
+
+  for (let i = 0; i < numFrames; i++) {
+    Object.entries(graph).forEach(([nodeID, node]) => {
+      const startPosn = nodeStartingPositions[nodeID];
+      const endPosn = nodeDestinations[nodeID];
+      const startVec = new THREE.Vector3(startPosn.x, startPosn.y, startPosn.z);
+      const endVec = new THREE.Vector3(endPosn.x, endPosn.y, endPosn.z);
+      const direction = new THREE.Vector3().subVectors(endVec, startVec);
+
+      node.posn.x += direction.x / numFrames;
+      node.posn.y += direction.y / numFrames;
+      node.posn.z += direction.z / numFrames;
+    });
+
+    yield graph;
+  }
+}
+
+const randomGraph = generateRandomGraph(10);
 const world = {
-  graph: exampleGraph,
+  graph: randomGraph,
 };
 
-var somethingHasChanged = true;
+let somethingHasChanged = true;
+
+let animationGenerator = randomizeGraphAnimation(world.graph);
 
 function init() {
   const scene = initScene();
@@ -69,20 +168,31 @@ function init() {
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    1000,
   );
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  camera.position.z = 50;
+  camera.position.z = 75;
 
   const controls = new OrbitControls(camera, renderer.domElement);
   const interaction = new Interaction(renderer, scene, camera);
+
+  const spotlight = initSpotlight();
+  const ground = initGroundMesh();
+
   const animate = () => {
+    const { value, done } = animationGenerator.next();
+    if (!done) {
+      world.graph = value;
+      somethingHasChanged = true;
+    }
     if (somethingHasChanged) {
       addGraphToScene(scene, world.graph);
+      scene.add(spotlight);
+      scene.add(ground);
       somethingHasChanged = false;
     }
 
@@ -103,7 +213,7 @@ function initScene() {
 
 function initSpotlight() {
   const spotLight = new THREE.SpotLight(0xffffff, 1);
-  spotLight.position.set(0, 0, 100);
+  spotLight.position.set(0, 100, 0);
 
   spotLight.castShadow = true;
 
@@ -120,7 +230,6 @@ function addGraphToScene(scene, graph) {
   while (scene.children.length) {
     scene.remove(scene.children[0]);
   }
-  scene.add(initSpotlight());
   Object.entries(graph).forEach(([nodeID, node]) => {
     addNodeToScene(scene, nodeID, node);
     addOutEdgesToScene(scene, graph, node);
@@ -142,7 +251,7 @@ function addNodeToScene(scene, nodeID, node) {
   }
 
   const sphere = new THREE.Mesh(geometry, material);
-  sphere.on("mousedown", () => updateNodeTexture(node));
+  sphere.on('mousedown', () => updateNodeTexture(node));
 
   scene.add(sphere);
 }
@@ -165,7 +274,7 @@ function addOutEdgesToScene(scene, graph, node) {
     const neighborVec = new THREE.Vector3(
       neighborPosn.x,
       neighborPosn.y,
-      neighborPosn.z
+      neighborPosn.z,
     );
     const direction = new THREE.Vector3().subVectors(neighborVec, nodeVec);
 
@@ -173,7 +282,7 @@ function addOutEdgesToScene(scene, graph, node) {
       CYLINDER_RADIUS,
       CYLINDER_RADIUS,
       direction.length(),
-      64
+      64,
     );
     const material = new THREE.MeshPhongMaterial();
     material.color = new THREE.Color(EDGE_COLOR);
@@ -182,14 +291,30 @@ function addOutEdgesToScene(scene, graph, node) {
 
     cylinder.quaternion.setFromUnitVectors(
       Y_AXIS,
-      direction.clone().normalize()
+      direction.clone().normalize(),
     );
     cylinder.position.copy(
-      new THREE.Vector3().addVectors(nodeVec, direction.multiplyScalar(0.5))
+      new THREE.Vector3().addVectors(nodeVec, direction.multiplyScalar(0.5)),
     );
 
     scene.add(cylinder);
   });
+}
+
+function initGroundMesh() {
+  const geometry = new THREE.PlaneGeometry(60, 60, 199, 199);
+  for (let i = 0; i < geometry.vertices.length; i++) {
+    geometry.vertices[i].z = terrainData[i] / 65535 * 25;
+  }
+  geometry.rotateX(-Math.PI / 2);
+  geometry.translate(0, -30, 0);
+
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x875b3b,
+    // wireframe: true,
+  });
+
+  return new THREE.Mesh(geometry, material);
 }
 
 init();
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.static('public'));
+app.use('/node_modules', express.static(`${__dirname}/node_modules`));
 app.get('/', (_, res) => res.sendFile('/public/index.html', { root: __dirname }));
 app.listen(3000);
