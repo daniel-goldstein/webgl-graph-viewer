45d735dbfb18468c4517d0f6f1f1a96613136aa4
Jason Crouse <jcrouse@drift.com>
2020-11-29 21:47:16 -0500

    click nodes to cycle between colors and textures

--- public/js/three-graph-viewer.js
+++ public/js/three-graph-viewer.js
@@ -1,19 +1,30 @@
 /* eslint no-use-before-define: 0 */
 
 import * as THREE from "./build/three.module.js";
+import { Interaction } from "./build/three-interaction.module.js";
 import { OrbitControls } from "./examples/jsm/controls/OrbitControls.js";
-import addKeyHandlers from "./KeyHandlers.js";
 
 const SPHERE_RADIUS = 5;
 const CYLINDER_RADIUS = 1;
 
 const Y_AXIS = new THREE.Vector3(0, 1, 0);
 
-const SCENE_COLOR = 0xfaf4da;
-const NODE_COLOR = 0x1987f8;
-const EDGE_COLOR = 0xe9533b;
-
 const BACKGROUND_FILEPATH = "space.jpeg";
+const STAINLESS_STEEL_FILEPATH = "stainless-steel.jpg";
+const LAVA_FILEPATH = "lava.jpg";
+
+const BLUE = 0x1987f8;
+const RED = 0xe9533b;
+const GREEN = 0x26ebaf;
+
+const EDGE_COLOR = RED;
+const NODE_TEXTURES = [
+  new THREE.Color(BLUE),
+  new THREE.Color(RED),
+  new THREE.Color(GREEN),
+  new THREE.TextureLoader().load(STAINLESS_STEEL_FILEPATH),
+  new THREE.TextureLoader().load(LAVA_FILEPATH),
+];
 
 const exampleGraph = {
   A: {
@@ -23,6 +34,7 @@ const exampleGraph = {
       y: 10,
       z: 10,
     },
+    texture: NODE_TEXTURES[0],
   },
   B: {
     neighbors: ["C"],
@@ -31,6 +43,7 @@ const exampleGraph = {
       y: 10,
       z: 0,
     },
+    texture: NODE_TEXTURES[0],
   },
   C: {
     neighbors: [],
@@ -39,8 +52,15 @@ const exampleGraph = {
       y: 0,
       z: -5,
     },
+    texture: NODE_TEXTURES[0],
   },
 };
+
+const world = {
+  graph: exampleGraph,
+};
+
+var somethingHasChanged = true;
 
 function init() {
   const scene = initScene();
@@ -56,14 +76,18 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  addGraph(scene, exampleGraph);
   camera.position.z = 50;
 
   const controls = new OrbitControls(camera, renderer.domElement);
-  addKeyHandlers(document);
+  const interaction = new Interaction(renderer, scene, camera);
   const animate = () => {
+    if (somethingHasChanged) {
+      addGraphToScene(scene, world.graph);
+      somethingHasChanged = false;
+    }
+
     requestAnimationFrame(animate);
-    // controls.update();
+    controls.update();
     renderer.render(scene, camera);
   };
   animate();
@@ -71,6 +95,13 @@ function init() {
 
 function initScene() {
   const scene = new THREE.Scene();
+
+  const texture = new THREE.TextureLoader().load(BACKGROUND_FILEPATH);
+  scene.background = texture;
+  return scene;
+}
+
+function initSpotlight() {
   const spotLight = new THREE.SpotLight(0xffffff, 1);
   spotLight.position.set(0, 0, 100);
 
@@ -82,33 +113,52 @@ function initScene() {
   spotLight.shadow.camera.near = 500;
   spotLight.shadow.camera.far = 4000;
   spotLight.shadow.camera.fov = 30;
-
-  scene.add(spotLight);
-
-  const texture = new THREE.TextureLoader().load(BACKGROUND_FILEPATH);
-  scene.background = texture;
-  return scene;
+  return spotLight;
 }
 
-function addGraph(scene, graph) {
-  Object.values(graph).forEach(({ neighbors, posn }) => {
-    addNode(scene, posn);
-    addOutEdges(scene, graph, posn, neighbors);
+function addGraphToScene(scene, graph) {
+  while (scene.children.length) {
+    scene.remove(scene.children[0]);
+  }
+  scene.add(initSpotlight());
+  Object.entries(graph).forEach(([nodeID, node]) => {
+    addNodeToScene(scene, nodeID, node);
+    addOutEdgesToScene(scene, graph, node);
   });
 }
 
-function addNode(scene, posn) {
+function addNodeToScene(scene, nodeID, node) {
+  const { posn, texture } = node;
   const geometry = new THREE.SphereGeometry(SPHERE_RADIUS, 32, 32);
   geometry.translate(posn.x, posn.y, posn.z);
 
   const material = new THREE.MeshPhongMaterial();
-  material.color = new THREE.Color(NODE_COLOR);
+  if (texture instanceof THREE.Color) {
+    material.map = undefined;
+    material.color = texture;
+  } else {
+    material.color = undefined;
+    material.map = texture;
+  }
 
   const sphere = new THREE.Mesh(geometry, material);
+  sphere.on("mousedown", () => updateNodeTexture(node));
+
   scene.add(sphere);
 }
 
-function addOutEdges(scene, graph, posn, neighbors) {
+function updateNodeTexture(node) {
+  node.texture = nextNodeTexture(node.texture);
+  somethingHasChanged = true;
+}
+
+function nextNodeTexture(nodeTexture) {
+  const nodeTextureIndex = NODE_TEXTURES.indexOf(nodeTexture);
+  return NODE_TEXTURES[(nodeTextureIndex + 1) % NODE_TEXTURES.length];
+}
+
+function addOutEdgesToScene(scene, graph, node) {
+  const { posn, neighbors } = node;
   const nodeVec = new THREE.Vector3(posn.x, posn.y, posn.z);
   neighbors.forEach((name) => {
     const neighborPosn = graph[name].posn;
