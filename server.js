afa36763d79a554abbac1c896009da673d7149d8
Daniel Goldstein <danielgold95@gmail.com>
2020-12-01 13:26:38 -0500

    listen on env port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static('public'));
-app.use('/node_modules', express.static(`${__dirname}/node_modules`));
-app.get('/', (_, res) => res.sendFile('/public/index.html', { root: __dirname }));
-app.listen(3000);
+app.use(express.static("public"));
+app.use("/node_modules", express.static(`${__dirname}/node_modules`));
+app.get("/", (_, res) =>
+  res.sendFile("/public/index.html", { root: __dirname })
+);
+app.listen(process.env.PORT || 3000);
